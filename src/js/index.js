e02b9b6304ad2b4ba5831cb94d1d16f8411454bc
mac <mac@MacBook-Pro-3.local>
2024-12-10 12:46:03 +0200

    initialise project

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,10 @@
 import hero from './components/hero';
-import modalLogic from './components/modalLogic';
-import sidebarLogic from "./components/sidebarLogic";
 import setupMegaMenu from "./components/setupMegaMenu";
 import toggleMenuVisibility from "./components/toggleMenuVisibility";
-import phoneInput from "./components/phoneInput";
-import langMenu from "./components/langMenu";
 
 
 document.addEventListener("DOMContentLoaded", function() {
     hero();
-    modalLogic();
     setupMegaMenu();
-    sidebarLogic();
     toggleMenuVisibility();
-    phoneInput();
-    langMenu();
 });
