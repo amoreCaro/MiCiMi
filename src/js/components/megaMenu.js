376a168dafeee84d29046abcb6c30bf1a8d94ad6
mac <mac@MacBook-Pro-3.local>
2024-12-21 20:06:57 +0200

    mega-menu-dropdown

---
+++ src/js/components/megaMenu.js
@@ -0,0 +1,11 @@
+export default function megaMenu() {
+  const hasChildren = $('.has-children');
+
+  hasChildren.on('click', function () {
+    const megaMenuDropdown = $(this).find('.mega__menu-dropdown');
+
+    if (megaMenuDropdown.length) {
+      megaMenuDropdown.toggleClass('show');
+    }
+  });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import news from './components/news';
 import sidebarLogic from './components/sidebarLogic';
+import megaMenu from './components/megaMenu';
 
 $( document ).ready(function() {
     news();
     sidebarLogic();
+    megaMenu();
 });
